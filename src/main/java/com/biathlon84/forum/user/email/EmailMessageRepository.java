507d9da157b029eb5f2e401218108e0646032224
biatlon84 <biatlon84@gmail.com>
2022-01-21 12:02:05 +0300

    initialization commit

--- src/main/java/com/biathlon84/forum/user/email/EmailMessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.biathlon84.forum.user.email;
-
-import com.biathlon84.forum.model.entity.EmailMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmailMessageRepository extends JpaRepository<EmailMessage, Integer> {
-    EmailMessage findById(int id);
-}
--- src/main/java/com/biathlon84/forum/userProfile/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biathlon84.forum.userProfile;
-
-import com.biathlon84.forum.model.entity.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile,Integer> {
-
-
-}
